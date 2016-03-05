c59c570ae1b400eb2a1f6fe95b2015be74133613
Dragan Marjanovic - Air <dmarj97@gmail.com>
2016-03-05 17:10:08 +1000

    Added test structure to view

--- backend/borealysis/views.py
+++ backend/borealysis/views.py
@@ -8,19 +8,70 @@ def index():
     return 'Hello World!'
 
 
+@app.route('/holes')
 @app.route('/holes/')
 def holes():
-    return ("these are the holes")
+    """ Returns Hole ID, Latitude, and Longitude"""
+    return ('''
+        {
+    "1": {
+        "location": {
+            "latitude": 123.456,
+            "longitude": 789.101
+        }
+    },
+    "2": {
+        "location": {
+            "latitude": 123.456,
+            "longitude": 789.101
+        }
+    }
+}
+        ''')
 
 
-@app.route('/holes/<id>')
-def hole_id():
-    return ("h")
+@app.route('/holes/<bore_id>')
+@app.route('/holes/<bore_id>/')
+def hole_id(bore_id):
+    """Returns JSON data related to the specifichole.
+
+    Arguments:
+    borehole_id -- The ID of the borehole
+
+    Returns:
+    location --
+        latitude -- coordinates
+        longitude -- 
+    seam_count -- 
 
 
-@app.route('/summary/<id>')
-def summary():
-    pass
+    """
+    return 'Bore_ID: %s' % bore_id
+
+
+@app.route('/holes/<bore_id>/<seam_id>')
+@app.route('/holes/<bore_id>/<seam_id>/')
+def seam(bore_id, seam_id):
+    """ """
+    return ("seam")
+
+
+@app.route('/summary/<bore_id>')
+@app.route('/summary/<bore_id>/')
+def summary(bore_id):
+    return('''
+{
+    "coal_count": 54,
+    "coal_percent": 73.5,
+    "rare": [{
+        "name": "ABBREVIATION",
+        "percent": 1
+    }, {
+        "name": "ABBREVIATION",
+        "percent": 2
+    }]
+}
+        ''')
 
 
 # POST Views

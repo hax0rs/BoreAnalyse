86fa6e291dbb01d3ed7cf09e927a5941bf648ac5
draganmarjanovic <gagalug13@gmail.com>
2016-03-06 12:18:23 +1000

    GET Boreholes Function
    GET boreholes function

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,42 +3,51 @@ angular.module('borealysisApp', [])
     var api_url = "http://unearthed.nicolaus.pw/";
     var borealysis = this;
 
-
+    borealysis.boreholes = {};
     borealysis.data = {};
 
+    function data_request (url) {
+        var result = $http({
+          method: 'GET',
+          url: url
+        }).then(function successCallback(response) {
+            return (response);
+          }, function errorCallback(response) {
+            console.log("GET error.");
+          });
+        return (result);
+    }
 
     borealysis.get_boreholes = function() {
         var url = api_url.concat("holes/");
-        $http({
-          method: 'GET',
-          url: url
-        }).then(function successCallback(response) {
-            console.log(response.data);
-          }, function errorCallback(response) {
-            console.log("GET error.");
-          });
+        var response = data_request(url);
+        console.log(response);
+        // $http({
+        //   method: 'GET',
+        //   url: url
+        // }).then(function successCallback(response) {
+        //     borealysis.boreholes = response.data;
+        //     console.log(borealysis.boreholes)
+        //   }, function errorCallback(response) {
+        //     console.log("GET error.");
+        //   });
     };
 
     borealysis.get_borehole_properties = function(borehole_id){
-        var url = api_url.concat("holes/").concat(borehole_id);
-        $http({
-          method: 'GET',
-          url: url
-        }).then(function successCallback(response) {
-            console.log(response.data);
-          }, function errorCallback(response) {
-            console.log("GET error.");
-          });
+        var url = api_url.concat("holes/").concat(borehole_id).concat("/");
+        data = data_request(url);
+        console.log(data);
+
+
     };
 
     borealysis.borehole_search = function(){
         var search = borealysis.search;
-
+        borealysis.get_borehole_properties(search);
 
     };
 
-    // get_boreholes();
-
+    borealysis.get_boreholes();
 
 
     borealysis.test = [{"text":"ayy lmao"}];

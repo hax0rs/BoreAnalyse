9457c4e0360c98920c6ca800a64af72af3b1b129
Joshua Riddell <joshua.riddell11@gmail.com>
2016-03-06 14:48:34 +1000

    thing

--- js/main.js
+++ js/main.js
@@ -3,42 +3,60 @@ angular.module('borealysisApp', [])
     var api_url = "http://unearthed.nicolaus.pw/";
     var borealysis = this;
 
-
+    borealysis.current = {};
+    borealysis.boreholes = {};
     borealysis.data = {};
 
 
+// Animation
+
+function lmao () {
+    var h = 100;
+var foo = setInterval(function () {
+   if (h>2000) {
+    clearInterval(foo);
+   }
+   h = h + 5;
+   document.getElementById('vis').style.height = h + 'px';
+}, 100);
+
+}
+
     borealysis.get_boreholes = function() {
         var url = api_url.concat("holes/");
+        var response = data_request(url);
+        console.log(response);
         $http({
           method: 'GET',
           url: url
         }).then(function successCallback(response) {
-            console.log(response.data);
+            borealysis.boreholes = response.data;
+            console.log(borealysis.boreholes);
           }, function errorCallback(response) {
             console.log("GET error.");
           });
     };
 
     borealysis.get_borehole_properties = function(borehole_id){
-        var url = api_url.concat("holes/").concat(borehole_id);
+        var url = api_url.concat("holes/").concat(borehole_id).concat("/");
         $http({
           method: 'GET',
           url: url
         }).then(function successCallback(response) {
-            console.log(response.data);
+            borealysis.current = response.data;
+            console.log(borealysis.current);
           }, function errorCallback(response) {
             console.log("GET error.");
           });
+
+
     };
 
     borealysis.borehole_search = function(){
         var search = borealysis.search;
-
-
+        borealysis.get_borehole_properties(search);
+        lmao();
     };
-
-    // get_boreholes();
-
 
 
     borealysis.test = [{"text":"ayy lmao"}];
@@ -46,5 +64,4 @@ angular.module('borealysisApp', [])
 
 
 });
-
 

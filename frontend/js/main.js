236dda0bd8b9fdae9bce5d597922b91ed38593dc
Dragan Marjanovic - Air <dmarj97@gmail.com>
2016-03-06 14:40:19 +1000

    segmentation loader

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,42 +3,56 @@ angular.module('borealysisApp', [])
     var api_url = "http://unearthed.nicolaus.pw/";
     var borealysis = this;
 
-
+    borealysis.current = {};
+    borealysis.boreholes = {};
     borealysis.data = {};
+
+
+// Animation
+var h = 100;
+var foo = setInterval(function () {
+   if (h>2000) {
+    clearInterval(foo); 
+   }
+   h = h + 5;
+   document.getElementById('vis').style.height = h + 'px';
+}, 100);
 
 
     borealysis.get_boreholes = function() {
         var url = api_url.concat("holes/");
+        var response = data_request(url);
+        console.log(response);
         $http({
           method: 'GET',
           url: url
         }).then(function successCallback(response) {
-            console.log(response.data);
+            borealysis.boreholes = response.data;
+            console.log(borealysis.boreholes)
           }, function errorCallback(response) {
             console.log("GET error.");
           });
     };
 
     borealysis.get_borehole_properties = function(borehole_id){
-        var url = api_url.concat("holes/").concat(borehole_id);
+        var url = api_url.concat("holes/").concat(borehole_id).concat("/");
         $http({
           method: 'GET',
           url: url
         }).then(function successCallback(response) {
-            console.log(response.data);
+            borealysis.current = response.data;
+            console.log(borealysis.current);
           }, function errorCallback(response) {
             console.log("GET error.");
           });
+
+
     };
 
     borealysis.borehole_search = function(){
         var search = borealysis.search;
-
-
+        borealysis.get_borehole_properties(search);
     };
-
-    // get_boreholes();
-
 
 
     borealysis.test = [{"text":"ayy lmao"}];
@@ -46,5 +60,4 @@ angular.module('borealysisApp', [])
 
 
 });
-
 
